0d330c34778a2ae7db7bb6e78ea3c670f9e517d4
Pegos <stelios@pegos.net>
2024-07-09 12:22:53 +0300

    Update config.lua

--- config.lua
+++ config.lua
@@ -2,19 +2,14 @@ Config = {}
 
 Config.BossGrade = 0
 Config.UnemployedJobName = 'unemployed'
-Config.UnemployedGrade = 0
+Config.UnemployedGrade = 1
+Config.Debug = true
 
 Config.Locations = {
     {
-        name = 'bennys',
-        coords = vec3(430.452759, -1026.108032, 27.846140),
+        name = 'police',
+        coords = vec3(447.0, -975.0, 31.0),
         type = 1,
         distance = 3
-    },
-    {
-        name = 'bloods',
-        coords = vec3(460.452759, -1026.108032, 27.846140),
-        type = 1,
-        distance = 3
-    },
+    }
 }
